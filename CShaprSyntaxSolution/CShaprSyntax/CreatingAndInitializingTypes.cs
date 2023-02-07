9d4dce3e60caaf326966ef4710749cc1e4724d92
Kevin Finucan <kev.finucan@gmail.com>
2023-02-07 16:22:09 -0500

    Day 2 complete

--- CShaprSyntaxSolution/CShaprSyntax/CreatingAndInitializingTypes.cs
+++ CShaprSyntaxSolution/CShaprSyntax/CreatingAndInitializingTypes.cs
@@ -1,4 +1,6 @@
-﻿namespace CSharpSyntax;
+﻿using System.Text;
+
+namespace CSharpSyntax;
 
 public class CreatingAndInitializingTypes
 {
@@ -32,6 +34,81 @@ public class CreatingAndInitializingTypes
         var simplified = new AnnoyinglyLongAndSpecificClassName();
 
     }
+
+    /* 
+       String builder is a great tool for use cases where a string will be
+       altered many times, or for alterations on very large strings.
+       This is because it will be a consistent object on the heap
+       vs standard, immutable strings which create copies and allocate 
+       additional space on the heal when they are altered. String builder is
+       more cumbersome to work with but more memory efficient then strings.
+    */
+    [Fact]
+    public void MutableStringsWithStringBuilder()
+    {
+        var message = new StringBuilder();
+
+        foreach(var num in Enumerable.Range(1, 10000))
+        {
+            message.Append(num.ToString() + ", ");
+        }
+    }
+
+    [Fact]
+    public void MoreAboutStrings()
+    {
+        var name = "Bob"; 
+        var age = 33;
+        var pay = 120_000.00M;
+
+        var message = "The name is " + name + " and the age is " + age + ".";
+        var message2 = string.Format("The name is {0} and the age is {1} (again, name: {0})", name, age);
+        var m3 = $"{name} makes {pay:c} a year";
+    }
+
+    [Fact]
+    public void DoingConversionsOnTypes()
+    {
+        string myPay = "70000.00";
+
+        // decimal payAsNumber = decimal.Parse(myPay); -null pointer potential
+
+        //parsing
+        if(decimal.TryParse(myPay, out decimal payAsNumber))
+        {
+            Assert.Equal(70000.00M, payAsNumber);
+        }
+        else
+        {
+            Assert.True(false);
+        }
+
+        var birthdate = DateTime.Parse("11/10/1990");
+        Assert.Equal(11, birthdate.Month);
+        Assert.Equal(10, birthdate.Day);
+        Assert.Equal(1990, birthdate.Year);
+
+    }
+
+    /* 
+     * Use case example:
+     *  while (true)
+{
+        Console.Write("How old are you?: ");
+        var enteredAge = Console.ReadLine();     
+        
+        if (int.TryParse(enteredAge, out int age)) 
+    {
+
+        Console.WriteLine($"You are {age} years old!");
+        break; 
+    }
+    else
+    {
+        System.Console.WriteLine("What are you, a moron! Enter a number, fool!");
+    }
+}
+     */
 
     public class AnnoyinglyLongAndSpecificClassName { }
 }
---
+++ Practices/DJBreakTimer/DJBreakTimer/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int breakLengthAsNumber;
+TimeSpan songLengthAsTimeSpan;
+
+while (true)
+{
+    Console.WriteLine("How many mintues is your break: ");
+    var breakLength = Console.ReadLine();
+    if (int.TryParse(breakLength, out breakLengthAsNumber))
+    {
+        break;
+    }
+}
+
+breakLengthAsNumber *= 60; 
+
+while (true)
+{
+    Console.WriteLine("How long is the song (minutes:seconds): ");
+    var songLength = "00:" + Console.ReadLine();
+    if (TimeSpan.TryParse(songLength, out songLengthAsTimeSpan))
+    {
+        break;
+    }
+}
+
+Thread.Sleep(3000);
+
+var totalMinutesSong = songLengthAsTimeSpan.Minutes;
+var totalSecondsSong = songLengthAsTimeSpan.Seconds; 
+
+totalMinutesSong *= 60; 
+var beginSongTime = breakLengthAsNumber - totalMinutesSong - totalSecondsSong; 
+
+var minutesLeft = beginSongTime / 60;
+var secondsLeft = beginSongTime % 60; 
+
+Console.WriteLine("\n" + DateTime.Now + " is the start of your break.");
+var currentTime = DateTime.Now; 
+
+//currentTime.AddMinutes(minutesLeft);
+//currentTime.AddSeconds(secondsLeft); 
+Console.WriteLine("Start the song after " + minutesLeft + " minutes and " + secondsLeft + " seconds.");
+//Console.WriteLine("\nYou should start your song at " + currentTime.AddMinutes(minutesLeft) + ":" + currentTime.AddSeconds(secondsLeft));
