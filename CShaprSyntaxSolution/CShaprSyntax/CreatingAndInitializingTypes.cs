1f254c788b8f24a778b7117a74e65e9131b628c4
Kevin Finucan <kev.finucan@gmail.com>
2023-02-07 12:26:04 -0500

    Day 2 midday push

---
+++ CShaprSyntaxSolution/CShaprSyntax/CreatingAndInitializingTypes.cs
@@ -0,0 +1,37 @@
+﻿namespace CSharpSyntax;
+
+public class CreatingAndInitializingTypes
+{
+    // Explicit variables can be defined at the class level
+    // Var can only be used on local variables within methods
+    string classLevelVariable = "IAmAccessibleByAllMethodsInClass";
+    
+    [Fact]
+    public void UsingLiteralsToCreateInstancesOfTypes()
+    {
+        // Local variables; All C# code must be within a data type: class
+        // All variables must be within a method, or property (execution context)
+        string myName = "Kevin";
+        int myAge = 32;
+        bool isLegalToDrink = myAge >= 21;
+
+        Assert.True(isLegalToDrink);
+        Assert.Equal(32, myAge);
+    }
+
+    [Fact]
+    public void ImplicitTypedLocalVariable()
+    {
+        // Var = local variables only & must be initialized when declared.
+        var myAge = 32;
+
+        decimal myPay = 70.0M; // twinsies
+        var alsoMyPay = 70.0M; // twinsies
+
+        // var use case
+        var simplified = new AnnoyinglyLongAndSpecificClassName();
+
+    }
+
+    public class AnnoyinglyLongAndSpecificClassName { }
+}
---
+++ day 2/Demo1/Demo1.UnitTests/SongTests.cs
@@ -0,0 +1,13 @@
+namespace Demo1.UnitTests;
+
+public class SongTests
+{
+    [Fact]
+    public void CanCreateASong()
+    {
+        var song = new Song("Fire it Up", "Modest Mouse");
+
+        Assert.Equal("Fire it Up", song.Title);
+        Assert.Equal("Modest Mouse", song.Artist);
+    }
+}
